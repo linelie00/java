1ec4b43c0c61f03124daa489ab5fb56dbe8341a4
mandu <78725815+linelie00@users.noreply.github.com>
2024-03-19 14:43:26 +0900

    Update 3week2.java

--- 3week2.java
+++ 3week2.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        int n;
+        while (true) {
+            n = scanner.nextInt();
+            if (n > 18 || n < 1)
+                System.out.print("-1\n");
+            else break;
+        }
 
         for (int i = n; i > 0; i--) {
             for(int j = 0; j < i; j++) {
