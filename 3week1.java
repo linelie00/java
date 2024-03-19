8a172fa409984f919a2964308b741b2e01a432e7
mandu <78725815+linelie00@users.noreply.github.com>
2024-03-19 13:48:16 +0900

    Create 3week1.java

---
+++ 3week1.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int n[][] = {{1},{1,2,3},{1},{1,2,3,4},{1,2}};
+
+        for (int i = 0; i < 5; i++) {
+            for(int j = 0; j < n[i].length; j++) {
+                System.out.print(n[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
